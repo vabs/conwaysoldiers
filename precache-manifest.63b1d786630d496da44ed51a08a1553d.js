ace70106b4d7138ab9642980be7934be820c089f
vabs <njoy.today@gmail.com>
2020-05-21 09:21:33 -0400

    Updates

---
+++ precache-manifest.63b1d786630d496da44ed51a08a1553d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96d4a8334bb6bfddba7a8f16e07628f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54fed99fab2bec73476a",
+    "url": "/static/css/main.4da129b4.chunk.css"
+  },
+  {
+    "revision": "bc669449df1f2309fa60",
+    "url": "/static/js/2.8611434d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8611434d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54fed99fab2bec73476a",
+    "url": "/static/js/main.e62199c2.chunk.js"
+  },
+  {
+    "revision": "7cae2690b7ae98805e6c",
+    "url": "/static/js/runtime-main.e12c32fd.js"
+  }
+]);
